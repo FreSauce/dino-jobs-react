bac26dd471fa92bbe377e7c1466be082cd4772fe
Shardul Kurdukar <24shardul@gmail.com>
2022-10-31 08:31:13 +0530

    home

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
-import { createStyles, Navbar, Group, Code } from '@mantine/core';
+import { useState } from "react";
+import { createStyles, Navbar, Group, Code } from "@mantine/core";
 import {
   IconBellRinging,
-	IconHome,
-	IconJob,
+  IconHome,
+  IconJob,
   IconUser,
   IconKey,
   IconSettings,
@@ -12,17 +12,20 @@ import {
   IconReceipt2,
   IconSwitchHorizontal,
   IconLogout,
-} from '@tabler/icons';
-import { MantineLogo } from '@mantine/ds';
+} from "@tabler/icons";
+import { MantineLogo } from "@mantine/ds";
+import { Link } from "react-router-dom";
 
 const useStyles = createStyles((theme, _params, getRef) => {
-  const icon = getRef('icon');
+  const icon = getRef("icon");
   return {
     header: {
       paddingBottom: theme.spacing.md,
       marginBottom: theme.spacing.md * 1.5,
       borderBottom: `1px solid ${
-        theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[4]
+          : theme.colors.gray[2]
       }`,
     },
 
@@ -30,44 +33,61 @@ const useStyles = createStyles((theme, _params, getRef) => {
       paddingTop: theme.spacing.md,
       marginTop: theme.spacing.md,
       borderTop: `1px solid ${
-        theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[4]
+          : theme.colors.gray[2]
       }`,
     },
 
     link: {
       ...theme.fn.focusStyles(),
-      display: 'flex',
-      alignItems: 'center',
-      textDecoration: 'none',
+      display: "flex",
+      alignItems: "center",
+      textDecoration: "none",
       fontSize: theme.fontSizes.sm,
-      color: theme.colorScheme === 'dark' ? theme.colors.dark[1] : theme.colors.gray[7],
+      color:
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[1]
+          : theme.colors.gray[7],
       padding: `${theme.spacing.xs}px ${theme.spacing.sm}px`,
       borderRadius: theme.radius.sm,
       fontWeight: 500,
 
-      '&:hover': {
-        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
-        color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+      "&:hover": {
+        backgroundColor:
+          theme.colorScheme === "dark"
+            ? theme.colors.dark[6]
+            : theme.colors.gray[0],
+        color: theme.colorScheme === "dark" ? theme.white : theme.black,
 
         [`& .${icon}`]: {
-          color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+          color: theme.colorScheme === "dark" ? theme.white : theme.black,
         },
       },
     },
 
     linkIcon: {
       ref: icon,
-      color: theme.colorScheme === 'dark' ? theme.colors.dark[2] : theme.colors.gray[6],
+      color:
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[2]
+          : theme.colors.gray[6],
       marginRight: theme.spacing.sm,
     },
 
     linkActive: {
-      '&, &:hover': {
-        backgroundColor: theme.fn.variant({ variant: 'light', color: theme.primaryColor })
-          .background,
-        color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
+      "&, &:hover": {
+        backgroundColor: theme.fn.variant({
+          variant: "light",
+          color: theme.primaryColor,
+        }).background,
+        color: theme.fn.variant({ variant: "light", color: theme.primaryColor })
+          .color,
         [`& .${icon}`]: {
-          color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
+          color: theme.fn.variant({
+            variant: "light",
+            color: theme.primaryColor,
+          }).color,
         },
       },
     },
@@ -75,20 +95,22 @@ const useStyles = createStyles((theme, _params, getRef) => {
 });
 
 const data = [
-  { link: '', label: 'Home', icon: IconHome },
-  { link: '', label: 'Jobs', icon: IconReceipt2 },
-  { link: '', label: 'Profile', icon: IconUser },
-  { link: '', label: 'Interview', icon: IconKey },
-  { link: '', label: 'Settings', icon: IconSettings },
+  { link: "", label: "Home", icon: IconHome },
+  { link: "", label: "Jobs", icon: IconReceipt2 },
+  { link: "", label: "Profile", icon: IconUser },
+  { link: "", label: "Interview", icon: IconKey },
+  { link: "", label: "Settings", icon: IconSettings },
 ];
 
 const SideBar = () => {
   const { classes, cx } = useStyles();
-  const [active, setActive] = useState('Billing');
+  const [active, setActive] = useState("Billing");
 
   const links = data.map((item) => (
-    <a
-      className={cx(classes.link, { [classes.linkActive]: item.label === active })}
+    <Link
+      className={cx(classes.link, {
+        [classes.linkActive]: item.label === active,
+      })}
       href={item.link}
       key={item.label}
       onClick={(event) => {
@@ -98,11 +120,11 @@ const SideBar = () => {
     >
       <item.icon className={classes.linkIcon} stroke={1.5} />
       <span>{item.label}</span>
-    </a>
+    </Link>
   ));
 
   return (
-    <Navbar fixed={true}  width={{ sm: 250 }} p="md" >
+    <Navbar fixed={true} width={{ sm: 250 }} p="md">
       <Navbar.Section grow>
         <Group className={classes.header} position="apart">
           <h1>Dino Jobs</h1>
@@ -111,18 +133,26 @@ const SideBar = () => {
       </Navbar.Section>
 
       <Navbar.Section className={classes.footer}>
-        <a href="#" className={classes.link} onClick={(event) => event.preventDefault()}>
+        <a
+          href="#"
+          className={classes.link}
+          onClick={(event) => event.preventDefault()}
+        >
           <IconSwitchHorizontal className={classes.linkIcon} stroke={1.5} />
           <span>Change account</span>
         </a>
 
-        <a href="#" className={classes.link} onClick={(event) => event.preventDefault()}>
+        <a
+          href="#"
+          className={classes.link}
+          onClick={(event) => event.preventDefault()}
+        >
           <IconLogout className={classes.linkIcon} stroke={1.5} />
           <span>Logout</span>
         </a>
       </Navbar.Section>
     </Navbar>
   );
-}
+};
 
 export default SideBar;
--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,12 +1,84 @@
-import React from 'react'
-import MainWrapper from '../components/MainWrapper'
+import { Button, Container, Group, Stack } from "@mantine/core";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-	return (
-		<MainWrapper>
-			<div>Home</div>
-		</MainWrapper>
-	)
-}
+  return (
+    <div>
+      <Container
+        className="pattern-bg"
+        fluid
+        sx={{
+          display: "flex",
+          height: "100vh",
+          width: "100vw",
+        }}
+      >
+        <Group
+          sx={{
+            width: "100%",
+          }}
+          grow
+        >
+          <Stack px="xl">
+            <h1
+              style={{
+                fontSize: "3.5rem",
+                fontWeight: "bold",
+                fontFamily: "Open Sans",
+                color: "#fff",
+              }}
+            >
+              Best Platform To Get
+              <br /> Your{" "}
+              <span
+                className="text"
+                style={{
+                  fontWeight: "900",
+                  fontSize: "3.4rem",
+                  textTransform: "uppercase",
+                  color: "#f44",
+                }}
+              >
+                <span style={{ fontSize: "3.9rem" }}>D</span>ream{" "}
+                <span style={{ fontSize: "3.9rem" }}>J</span>ob
+              </span>
+            </h1>
+            <p
+              style={{
+                fontSize: "1.2rem",
+                color: "#ccc",
+              }}
+            >
+              With DinoJobs, you can find job that suits your skills and <br />
+              experience. We have a lot of job opportunities for you. Also,{" "}
+              <br />
+              we provide the best interview scheduling and handling system.
+            </p>
+            <Group py="md">
+              <Button variant="filled" color="white" size="lg">
+                <Link
+                  to="/login"
+                  style={{ textDecoration: "none", color: "#eee" }}
+                >
+                  Get Hired
+                </Link>
+              </Button>
+              <Button variant="light" color="white" size="lg">
+                <Link
+                  to="/recruiter/login"
+                  style={{ textDecoration: "none", color: "#eee" }}
+                >
+                  Hire Talents
+                </Link>
+              </Button>
+            </Group>
+          </Stack>
+          <Stack></Stack>
+        </Group>
+      </Container>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
